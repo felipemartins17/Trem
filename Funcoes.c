3ab7cacc4c067f67d75dffbf6d849eb06c0ae2d8
felipemartins17 <50057925+felipemartins17@users.noreply.github.com>
2019-06-24 21:42:21 -0300

    Update Funcoes.c

--- Funcoes.c
+++ Funcoes.c
@@ -1,49 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "headers.h"
+#define TamMax 3
 
 
-
-
-fila *cria_fila()
+fila *cria_fila ()
 {
-    fila *novo = (fila *)malloc(sizeof(fila));
+    fila *novo = (fila *) malloc (sizeof (fila));
     novo->ini = novo->fim = NULL;
     return novo;
 }
 
-pilha *cria_pilha()
+pilha *cria_pilha ()
 {
-    pilha *novo = (pilha *)malloc(sizeof(pilha));
+    pilha *novo = (pilha *) malloc (sizeof (pilha));
     novo->topo = NULL;
     return novo;
 }
 
-vagao *cria_vagao(int id)
+vagao *cria_vagao (int id)
 {
-    vagao *novo = (vagao *)malloc(sizeof(vagao));
+    vagao *novo = (vagao *) malloc (sizeof (vagao));
     novo->prox = NULL;
     novo->id = id;
     return novo;
 }
 
-lista* cria_lista (void)
+lista *cria_lista (void)
 {
-return NULL;
+    return NULL;
 }
 
-lista* insere_lista (lista* l, int id)
+lista *insere_lista (lista * l, int id)
 {
-lista* novo =(lista*)malloc(sizeof(lista));
-novo->info = id;
-novo->prox = l;
-return novo;
+    lista *novo = (lista *) malloc (sizeof (lista));
+    novo->info = id;
+    novo->prox = l;
+    return novo;
 }
 
-void insere_fila(fila *f, int id)
+void insere_fila (fila * f, int id)
 {
-    vagao *novo = cria_vagao(id);
-    if(f->fim == NULL)
+    vagao *novo = cria_vagao (id);
+    if (f->fim == NULL)
     {
         f->ini = f->fim = novo;
     }
@@ -54,23 +53,22 @@ void insere_fila(fila *f, int id)
     }
 }
 
-void libera_fila(fila *f)
+void libera_fila (fila * f)
 {
     vagao *aux;
-    while(f->ini != NULL)
+    while (f->ini != NULL)
     {
         aux = f->ini;
         f->ini = f->ini->prox;
-        free(aux);
+        free (aux);
     }
-
-    free(f);
+    free (f);
 }
 
-void push(pilha *p, int id)
+void push (pilha * p, int id)
 {
-    vagao *novo = cria_vagao(id);
-    if(p->topo == NULL)
+    vagao *novo = cria_vagao (id);
+    if (p->topo == NULL)
     {
         p->topo = novo;
     }
@@ -81,11 +79,10 @@ void push(pilha *p, int id)
     }
 }
 
-void pop(pilha *p)
+void pop (pilha * p)
 {
     vagao *aux;
-
-    if(p->topo == NULL)
+    if (p->topo == NULL)
     {
         return;
     }
@@ -93,49 +90,43 @@ void pop(pilha *p)
     {
         aux = p->topo;
         p->topo = p->topo->prox;
-        free(aux);
+        free (aux);
         return;
     }
 }
 
-void libera_pilha(pilha *p)
+void libera_pilha (pilha * p)
 {
     vagao *aux;
-
-    if(p == NULL)
+    if (p == NULL)
         return;
-
-    while(p->topo != NULL)
+    while (p->topo != NULL)
     {
         aux = p->topo;
         p->topo = p->topo->prox;
-        free(aux);
+        free (aux);
     }
-
-    free(p);
+    free (p);
 }
 
-void retira_vagao(fila *f)
+void retira_vagao (fila * f)
 {
     vagao *aux;
-
     aux = f->ini;
     f->ini = f->ini->prox;
-    free(aux);
+    free (aux);
 }
 
-void imprime_fila(fila *f)
+void imprime_fila (fila * f)
 {
-
     while (f->ini->prox != NULL)
     {
-        printf("%d\t", f->ini->id);
+        printf ("%d\t", f->ini->id);
         f->ini = f->ini->prox;
     }
-
 }
 
-int checa_repetido (fila *f, int id)
+int checa_repetido (fila * f, int id)
 {
     while (id != NULL)
     {
@@ -147,9 +138,17 @@ int checa_repetido (fila *f, int id)
         else
             f->ini = f->ini->prox;
     }
-return 0;
-
+    return 0;
 }
+
+void pilha_cheia(pilha* plh)
+{
+    if (plh->topo == TamMax)
+    {
+        printf("Pilha cheia\n");
+    }
+}
+
 
 
 
